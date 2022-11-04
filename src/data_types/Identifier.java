cdc7cc82bd4ce19fff31e2509bdb6026176770dc
JesusPayan <jesus.he.payan@gmail.com>
2022-11-03 22:12:56 -0700

    adding Indentifier Example

---
+++ src/data_types/Identifier.java
@@ -0,0 +1,21 @@
+package data_types;
+
+public class Identifier {
+    public static void main(String[] args) {
+        //Primitive DataTypes
+        int itsAnIntVariable = 10; /*Correct*/
+        int ItsanIntVariable = 10; /*Incorrect*/
+        double $tsADoubleVariable = 1.302;
+        float 572tsAFloatVariable = 12.3f;
+        long _its_ALong_Variable = 1000000;
+     
+        //Objects
+        String coolString = "CoolString";
+
+        CoolClass coolClassInstance = new CoolClass();
+        System.out.println(coolClassInstance.name);
+    }
+}
+class CoolClass{
+    String name = "This is a coolClass";
+}
