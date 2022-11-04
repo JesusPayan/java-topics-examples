862e5ee924858df7e67b75847fafd883f3c2b268
JesusPayan <jesus.he.payan@gmail.com>
2022-11-03 22:25:00 -0700

    Adding Player and Monster Class

---
+++ src/data_types/play_ground/WeakMonster.java
@@ -0,0 +1,7 @@
+package data_types.play_ground;
+/*TODO: Create all atributes to a WeakMonster
+ *  Name, Race, Skills, Level,Coins,
+ *  ,Speed alive?
+ * */
+public class WeakMonster {
+}
---
+++ src/data_types/player/Player.java
@@ -0,0 +1,7 @@
+package data_types.player;
+/*TODO: Create all atributes to a player
+*  Name, Vocation, Skills, HealhLevel,MagicLevel,
+*  Level, Speed, Promotion?  */
+public class Player {
+
+}
